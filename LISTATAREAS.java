35cc554e0da5df94a1d9f1c9fe3279a3ce1f8db5
Eve4804 <143903874+Eve4804@users.noreply.github.com>
2023-09-05 09:46:37 -0500

    Add files via upload

---
+++ LISTATAREAS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Lista de tareas:");
+        String t1 = "Imprimir imagenes";
+        String t2 = "Hacer ensayo";
+        String t3 = "Linea del tiempo";
+        String t4 = "Mapa conceptual";
+        String t5 = "Crucigrama";
+        
+        System.out.println("Tarea 1: " + t1);
+        System.out.println("Tarea 2:" + t2);
+        System.out.println("Tarea 3: " + t3);
+        System.out.println("Tarea 4: " + t4);
+        System.out.println("Tarea 5: " + t5);
+    }
+    
+}
